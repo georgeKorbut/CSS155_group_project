13c9a92b0ba535fb975b293f51fa78077f465de5
George Korbut <gkorbut@Georges-MacBook-Air.local>
2025-10-05 14:13:23 -0400

    return grade letter

--- CSS155_group_project.cpp
+++ CSS155_group_project.cpp
@@ -10,7 +10,44 @@
 #include <string>;
 using namespace std;
 
+char letterGrade(float);
+
 int main(){
-    
+
+    string studentName;
+    int grade1;
+    int grade2;
+    int grade3;
+    int grade4;
+    int grade5;
+
+    float gradeAverage;
+
+    ifstream inFile;
+    inFile.open(grade_input_file.dat);
+
+    if (!inFile){
+        cout << "Error opening file";
+        return 0;
+    }
+
+
+
+
+
+    inFile.close();
 }
 
+char letterGrade(float average){
+    if (average >= 90){
+        return 'A';
+    } else if (average >= 80 && average < 90){
+        return 'B';
+    } else if (average >= 70 && average < 80){
+        return 'C';
+    } else if (average >= 60 && average < 70){
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
